8a3d539c85fcf48c3f88151f9c3bfbfdfbdc0470
asaninocente <asaninocente@gmail.com>
2024-05-14 13:45:54 +0200

    ListView

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,10 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .forms import ReviewForm
 from .models import Review
@@ -61,14 +63,15 @@ def thank_you(request):
   return render(request, 'reviews/thank_you.html')
 """
 
-class ReviewsListView(TemplateView):
+class ReviewsListView(ListView):
   template_name = 'reviews/review_list.html'
+  model = Review
+  context_object_name = 'reviews'
 
-  def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    reviews = Review.objects.all()
-    context['reviews'] = reviews
-    return context
+  # def get_queryset(self):
+  #   base_query = super().get_queryset()
+  #   data = base_query.filter(rating__gt=3)
+  #   return data
 
 class SingleReviewView(TemplateView):
   template_name = 'reviews/single_review.html'
