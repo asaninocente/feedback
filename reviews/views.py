cf69e8069ed325d2c3d01fa031df653166ca4805
asaninocente <asaninocente@gmail.com>
2024-05-14 12:12:39 +0200

    Saving Data with a Modelform

--- reviews/views.py
+++ reviews/views.py
@@ -2,17 +2,19 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import ReviewForm
-from .models import Review
+## from .models import Review
 
 # Create your views here.
 
 def review(request):
   if request.method == 'POST':
-    form = ReviewForm(request.POST)
+    ## existing_data = Review.objects.get(pk=1)
+    form = ReviewForm(request.POST) ## instance=existing_data
 
     if form.is_valid():
-      review = Review(user_name=form.cleaned_data['user_name'], review_text=form.cleaned_data['review_text'], rating=form.cleaned_data['rating'])
-      review.save()
+      form.save()
+      # review = Review(user_name=form.cleaned_data['user_name'], review_text=form.cleaned_data['review_text'], rating=form.cleaned_data['rating'])
+      # review.save()
       return HttpResponseRedirect('/thank-you')
   else:
     form = ReviewForm()
